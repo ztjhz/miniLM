78f2b5da8ac3b8e76f07a2c1d0ebb35270591f7d
Jing Hua <tohjinghua123@gmail.com>
2023-11-04 13:16:10 +0800

    add yelp dataset

--- pipeline/sliced_roberta.py
+++ pipeline/sliced_roberta.py
@@ -1,5 +1,7 @@
 import sys, pathlib; sys.path.append(str(pathlib.Path(__file__).parents[1]))
 
+import argparse
+
 import json
 
 import deepspeed
@@ -13,24 +15,47 @@ from datasets import load_dataset
 from transformers import EvalPrediction
 
 from models.SlicedRoberta import SlicedRoberta, compute_loss
-from utils.preprocessing import tokenize, train_val_test_split
+from utils.preprocessing import tokenize, train_val_test_split, subset_dataset
 from utils.trainer import compute_metrics
 
 NUM_EPOCH = 3
 
 def main():
+    parser = argparse.ArgumentParser(description='Sliced model experiment')
+    parser.add_argument("--dataset", choices=['imdb', 'yelp'], default='imdb', help="Dataset to use")
+    
+    # for deepspeed
+    parser.add_argument("--local_rank")
+
+    args = parser.parse_args()
+
+    # set up dataset
+    if args.dataset == 'imdb':
+        dataset = load_dataset("imdb")
+        num_labels = 2
+    elif args.dataset == 'yelp':
+        dataset = load_dataset("yelp_review_full")
+        num_labels = 5
+    else:
+        raise NotImplementedError
+
     # set up model
     with open("ds_config_roberta.json", "r") as f:
         df_config = json.load(f)
-    model = SlicedRoberta(num_labels=2)
+    model = SlicedRoberta(num_labels=num_labels)
     model_engine, _, _, _ = deepspeed.initialize(model=model,
                                                 model_parameters=model.parameters(),
                                                 config=df_config)
-    
-    # set up dataset
-    dataset = load_dataset("imdb")
+
     tokenized_datasets = tokenize(dataset, "roberta-base")
     train_dataset, val_dataset, test_dataset = train_val_test_split(tokenized_datasets)
+
+    if args.dataset == 'yelp':
+        # yelp dataset has 650k train which would take very long to train
+        # so we subset it
+        train_dataset = subset_dataset(train_dataset, size=25_000, seed=42)
+        val_dataset = subset_dataset(val_dataset, size=25_000, seed=42)
+        test_dataset = subset_dataset(test_dataset, size=25_000, seed=42)
 
     # set up dataloader
     train_dataloader = DataLoader(train_dataset, batch_size=df_config["train_batch_size"])
@@ -38,11 +63,12 @@ def main():
     test_dataloader = DataLoader(test_dataset, batch_size=df_config["train_batch_size"])
 
     # start wandb tracking
+    run_name = f"Sliced Roberta - {args.dataset}"
     if model_engine.global_rank == 0:
         wandb.init(
             project="text-sentiment-analysis",
             entity="sc4001",
-            name="Sliced Roberta",
+            name=run_name,
             config=df_config
             )
 
--- scripts/sliced_roberta.sh
+++ scripts/sliced_roberta.sh
@@ -1,3 +1,4 @@
 WANDB_API_KEY=your_wandb_api_key \
     deepspeed --include localhost:2,3 \
-    pipeline/sliced_roberta.py
+    pipeline/sliced_roberta.py \
+    --dataset imdb
